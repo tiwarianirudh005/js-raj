29915941156dd9d0bc94859cb97d036b3b9f8c57
Anirudh <154081637+tiwarianirudh005@users.noreply.github.com>
2025-02-10 17:07:50 +0000

    datatypes

---
+++ 1basic/02basdatatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)// we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("hitesh")
+
+let name ="anirudhfusze"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // null is object
+
+
