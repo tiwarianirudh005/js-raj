8ab245358851be362d83145292d36dc939e86b31
Anirudh <154081637+tiwarianirudh005@users.noreply.github.com>
2025-02-11 17:24:11 +0000

    conversion operations

---
+++ 1basic/03basconversionoperation.js
@@ -0,0 +1,30 @@
+//let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN
+// TRUE => 1; FALSE => 0
+
+//let isLoggedIn = 0;
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn); // Output: false
+
+//let isLoggedIn = 1;
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn); // Output: true
+
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
