439fd54ce073d797f06fdb4c19970f95248b38f7
Anirudh <154081637+tiwarianirudh005@users.noreply.github.com>
2025-04-01 17:13:28 +0000

    stringdone

---
+++ 1basic/05string.js
@@ -0,0 +1,38 @@
+const Name = "ANIRUDH";
+const repocount = "20";
+
+console.log(`Hello, my name is ${Name} and my repo count is ${repocount}.`);
+
+
+const gameName = new String('anirudh-js')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-9.4)
+console.log(anotherString);
+
+const newStringOne = "     hitesh    "
+
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://anirudh.com/anirudh20%tiwari"
+
+console.log(url.replace('20%', '-'));
+
+console.log(url.includes('hitesh'));
+console.log(url.includes('anirudh'));
+console.log(gameName.split('-'));
+
+
+
+
