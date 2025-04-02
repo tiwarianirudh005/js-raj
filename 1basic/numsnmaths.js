0f1917a92556a8df2f53eae6854effec53331acd
Anirudh <154081637+tiwarianirudh005@users.noreply.github.com>
2025-04-02 17:02:06 +0000

    math

---
+++ 1basic/numsnmaths.js
@@ -0,0 +1,42 @@
+//const score = 700
+//console.log(700);
+
+//const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+//const otherNumber = 123.8966
+
+//console.log(otherNumber.toPrecision(3));
+//console.log(otherNumber.toPrecision(6));
+
+//const hundreds = 100000
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++maths+++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.8));
+
+// console.log(Math.min(4,1,0,5,325,));
+// console.log(Math.max(4,1,0,5,325,));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
